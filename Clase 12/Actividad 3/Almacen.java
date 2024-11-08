84124bb7c93d3080ac9192d8212ba6f2c65952f0
Marcos Pirchio <Pirchiomarcos@gmail.com>
2024-11-08 17:25:12 -0300

    Create Almacen.java

---
+++ Clase 12/Actividad 3/Almacen.java
@@ -0,0 +1,21 @@
+package clase12.actividad3;
+
+public class Almacen {
+    private final int id;
+    private final String nombre;
+
+    public Almacen(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+}
