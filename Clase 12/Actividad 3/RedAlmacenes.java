d94ceba2853917bc42e65bf8a3a27034c4664cd3
Marcos Pirchio <Pirchiomarcos@gmail.com>
2024-11-08 17:35:25 -0300

    Create RedAlmacenes.java

---
+++ Clase 12/Actividad 3/RedAlmacenes.java
@@ -0,0 +1,22 @@
+package clase12.actividad3;
+
+public class RedAlmacenes {
+    public static void main(String[] args) {
+        Grafo redAlmacenes = new Grafo();
+
+        // Creamos almacenes
+        Almacen almacen1 = new Almacen(1, "Almacen A");
+        Almacen almacen2 = new Almacen(2, "Almacen B");
+        Almacen almacen3 = new Almacen(3, "Almacen C");
+        Almacen almacen4 = new Almacen(4, "Almacen D");
+
+        // Agregamos almacenes al grafo
+        redAlmacenes.agregarAlmacen(almacen1);
+        redAlmacenes.agregarAlmacen(almacen2);
+        redAlmacenes.agregarAlmacen(almacen3);
+        redAlmacenes.agregarAlmacen(almacen4);
+
+       
+    }
+}
+
