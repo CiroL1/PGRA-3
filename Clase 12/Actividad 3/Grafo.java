368856d7924db6bd8f8fdcc1e4749c746d1f1d65
Marcos Pirchio <Pirchiomarcos@gmail.com>
2024-11-08 17:31:53 -0300

    Update Grafo.java
    class update

--- Clase 12/Actividad 3/Grafo.java
+++ Clase 12/Actividad 3/Grafo.java
@@ -16,3 +16,14 @@ public class Grafo {
         almacenes.put(almacen.getId(), almacen);
         adyacencias.putIfAbsent(almacen.getId(), new ArrayList<>());
     }
+    // conectar dos almacenes entre si
+    public void conectarAlmacenes(int idAlmacen1, int idAlmacen2) {
+        if (almacenes.containsKey(idAlmacen1) && almacenes.containsKey(idAlmacen2)) {
+            adyacencias.get(idAlmacen1).add(idAlmacen2);
+            adyacencias.get(idAlmacen2).add(idAlmacen1); // Bidireccional
+        } else {
+            System.out.println("Uno o los dos almacenes no existen.");
+        }
+    }
+    
+}
