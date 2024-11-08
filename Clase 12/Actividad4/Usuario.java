996a49d347d6224276f44c51edade414a19798b6
Marcos Pirchio <Pirchiomarcos@gmail.com>
2024-11-08 17:43:44 -0300

    Create Usuario.java

---
+++ Clase 12/Actividad4/Usuario.java
@@ -0,0 +1,21 @@
+package clase12.actividad4;
+
+
+public class Usuario {
+    private final int id;
+    private final String nombre;
+
+    public Usuario(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
+
